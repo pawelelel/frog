8d4b972e72719d9e8ccf93804245513f2e50e4c1
PAWEL\pawci <pawel.leczkowski2005@gmail.com>
2024-11-20 21:50:00 +0100

    Cars work

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <conio.h>
-#include <cstdlib>
+#include <time.h>
+#include <stdlib.h>
 #include <curses.h>
+
+enum Colors
+{
+	Green = 1,
+	Red = 2
+};
+
+struct Car
+{
+	int x, y;
+	/*
+	 0 => left
+	 1 => right
+	*/
+	int direction;
+	int speed;
+};
 
 struct InitVariables
 {
 	int numberOfStreets;
 	int width;
 	int initPosition;
+	int numberOfCars;
 };
 
 struct CursesVariables
@@ -23,11 +42,49 @@ struct GameVariables
 	bool run;
 
 	int frogX, frogY;
+
+	Car* cars;
+	int carsSize;
+
+	int frames;
+	double time;
 };
+
+void MoveCars(GameVariables& game)
+{
+	for (int i = 0; i < game.carsSize; ++i)
+	{
+		if (game.frames % game.cars[i].speed == 0)
+		{
+			switch (game.cars[i].direction)
+			{
+				case 0:
+				{
+					game.cars[i].x--;
+					if (game.cars[i].x < 0)
+					{
+						game.cars[i].x = game.width - 1;
+					}
+					break;
+				}
+				case 1:
+				{
+					game.cars[i].x++;
+					if (game.cars[i].x > game.width - 1)
+					{
+						game.cars[i].x = 0;
+					}
+					break;
+				}
+			}
+		}
+	}
+}
 
 void PrintGameFrame(GameVariables &game)
 {
 	clear();
+	move(1, 0);
 
 	for (int i = 0; i < game.height; ++i)
 	{
@@ -37,40 +94,57 @@ void PrintGameFrame(GameVariables &game)
 		}
 		printw("\n");
 	}
-	attr_on( COLOR_PAIR(1), NULL );
-	move(game.frogY, game.frogX);
+
+	attr_on(COLOR_PAIR(Red), NULL);
+	for (int i = 0; i < game.carsSize; ++i)
+	{
+		move(game.cars[i].y + 1, game.cars[i].x);
+		printw("C");
+	}
+	attr_off(COLOR_PAIR(Red), NULL);
+
+	attr_on( COLOR_PAIR(Green), NULL );
+	move(game.frogY + 1, game.frogX);
 	printw("F");
 	attr_off(COLOR_PAIR(1), NULL);
 }
 
+void CheckCollision(GameVariables& game)
+{
+	for (int i = 0; i < game.carsSize; ++i)
+	{
+		if (game.cars[i].x == game.frogX && game.cars[i].y == game.frogY)
+		{
+			game.run = false;
+		}
+	}
+}
+
 void GameFrame(GameVariables &game, CursesVariables & curses)
 {
-	/*for (int i = 0; i < game.height; ++i)
-	{
-		for (int j = 0; j < game.width; ++j)
-		{
-			game.screen[i][j] = game.input;
-		}
-	}*/
-
+	game.frames++;
+	MoveCars(game);
+	CheckCollision(game);
 	PrintGameFrame(game);
-	wrefresh(curses.win);
 }
 
 bool Init(GameVariables& game, CursesVariables& curses)
 {
-	InitVariables init = {5, 10, 2 };
+	srand(time(NULL));
+	InitVariables init = {10, 50, 2, 20 };
 
+	// frog init
 	game.frogX = init.initPosition;
 	game.frogY = init.numberOfStreets * 2;
 
-
+	// other variables
 	game.run = true;
 	game.height = init.numberOfStreets * 2 + 1;
 	game.width = init.width;
+	game.frames = 0;
 
+	// screen
 	game.screen = new char*[game.height];
-
 	for (int i = 0; i < game.height; ++i)
 	{
 		game.screen[i] = new char[game.width];
@@ -87,22 +161,40 @@ bool Init(GameVariables& game, CursesVariables& curses)
 		}
 	}
 
+	// cars
+	game.carsSize = init.numberOfCars;
+	game.cars = new Car[game.carsSize];
+	for (int i = 0, j = 1; i < game.carsSize; ++i, j += 2)
+	{
+		game.cars[i].direction = rand() % 2;
+		game.cars[i].speed = (rand() % 4 + 1) * 100;
+		game.cars[i].x = rand() % game.width;
+
+		int random = rand() % init.numberOfStreets * 2;
+		if (random % 2 == 0)
+		{
+			random++;
+		}
+		game.cars[i].y = random;
+	}
+
 	// curses init
 	curses.win = initscr();
-	curs_set(0);
-	keypad(stdscr, TRUE);
-	nonl();
-	raw();
-	noecho();
-	notimeout(stdscr, TRUE);
-	nodelay(curses.win, TRUE);
+	//curs_set(0);
+	//keypad(stdscr, TRUE);
+	//nonl();
+	//raw();
+	//noecho();
+	//notimeout(stdscr, TRUE);
+	//nodelay(curses.win, TRUE);
 
 	// colors init
 	curses.colors = has_colors();
 	if (curses.colors)
 	{
 		start_color();
-		init_pair(1, COLOR_GREEN, COLOR_BLACK);
+		init_pair(Green, COLOR_GREEN, COLOR_BLACK);
+		init_pair(Red, COLOR_RED, COLOR_BLACK);
 	}
 
 	return true;
@@ -112,8 +204,14 @@ void EndProgram(GameVariables& game)
 {
 	for (int i = 0; i < game.width; ++i)
 	{
-		delete[] game.screen[i];
+		//delete[] game.screen[i];
 	}
+
+	clear();
+	printw("Koniec gry");
+	printw("Czas gry: %.2lfs", game.time);
+	
+
 	endwin();
 }
 
@@ -168,12 +266,18 @@ int main()
 		EndProgram(game);
 		return -1;
 	}
-	
 
+	clock_t tic = clock();
 	while (game.run)
 	{
 		GameFrame(game, curses);
 		HandleInput(game, curses);
+
+		clock_t toc = clock();
+		move(0, 0);
+		game.time = (double)(toc - tic) / CLOCKS_PER_SEC;
+		printw("Czas gry: %.2lfs", game.time);
+		wrefresh(curses.win);
 	}
 
 	EndProgram(game);
